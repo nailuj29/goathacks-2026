b5c4ee77ec11036b9e9c5a9776c7814558d46f30
Nailuj <me@nailuj.dev>
2026-01-16 22:51:32 -0500

    Refactor into folders

--- apps/backend/src/users/users.module.ts
+++ apps/backend/src/users/users.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from 'src/database.module';
+import { DatabaseModule } from 'src/database/database.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { usersProviders } from './users.provider';
