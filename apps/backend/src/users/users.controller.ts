4460f5c8dd06b2572f26ed0dcf0aaef1114bab3e
Nailuj <me@nailuj.dev>
2026-01-16 22:47:24 -0500

    Login implementation. It somehow worked first try. I don't believe it either

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -1,13 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
+  @Post('register')
   async register(@Body() registerUserDto: RegisterUserDto) {
     return await this.usersService.create(registerUserDto);
+  }
+
+  @Post('login')
+  async login(@Body() loginUserDto: LoginUserDto) {
+    return await this.usersService.login(loginUserDto);
   }
 }
--- apps/backend/src/users/users.service.ts
+++ apps/backend/src/users/users.service.ts
@@ -1,9 +1,15 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Model } from 'mongoose';
 import { User } from './interfaces/user.interface';
 import bcrypt from 'node_modules/bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -33,5 +39,33 @@ export class UsersService {
         process.env['JWT_SEC']!,
       ),
     };
+  }
+
+  async login({
+    username,
+    password,
+  }: LoginUserDto): Promise<{ token: string }> {
+    const user = await this.userModel
+      .find({
+        username,
+      })
+      .exec();
+
+    if (user.length == 0) {
+      throw new NotFoundException();
+    }
+
+    if (await bcrypt.compare(password, user[0].hashedPass)) {
+      return {
+        token: sign(
+          {
+            id: user[0]._id,
+          },
+          process.env['JWT_SEC']!,
+        ),
+      };
+    } else {
+      throw new ForbiddenException();
+    }
   }
 }
