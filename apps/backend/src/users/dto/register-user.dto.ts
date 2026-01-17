2f6d35f2963bd70557da370d232e9d220b30fcdb
Nailuj <me@nailuj.dev>
2026-01-16 23:55:00 -0500

    Add Swagger Docs

--- apps/backend/src/users/dto/register-user.dto.ts
+++ apps/backend/src/users/dto/register-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class RegisterUserDto {
+  @ApiProperty()
   readonly username: string;
+
+  @ApiProperty()
   readonly name: string;
+
+  @ApiProperty()
   readonly password: string;
 }
--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -5,21 +5,26 @@ import { LoginUserDto } from './dto/login-user.dto';
 import type { Request } from 'express';
 import { AuthGuard } from 'src/auth/auth.guard';
 import { ApiUser } from './interfaces/api-user.interface';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Register the user' })
   @Post('register')
   async register(@Body() registerUserDto: RegisterUserDto) {
     return await this.usersService.create(registerUserDto);
   }
 
+  @ApiOperation({ summary: 'Log in the user' })
   @Post('login')
   async login(@Body() loginUserDto: LoginUserDto) {
     return await this.usersService.login(loginUserDto);
   }
 
+  @ApiOperation({ summary: "Get the currently logged-in user's information" })
+  @ApiBearerAuth()
   @Get('me')
   @UseGuards(AuthGuard)
   info(@Req() req: Request & { user: ApiUser }) {
