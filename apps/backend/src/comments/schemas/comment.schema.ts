fd6736e6cd08194f4d251a37e60006e1ea2ef0a6
Nailuj <me@nailuj.dev>
2026-01-17 19:36:22 -0500

    I love copy-paste

--- apps/backend/src/comments/schemas/comment.schema.ts
+++ apps/backend/src/comments/schemas/comment.schema.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from 'mongoose';
 
-export const PostSchema = new mongoose.Schema({
+export const CommentSchema = new mongoose.Schema({
   text: { type: String, required: true },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   post: { type: Schema.Types.ObjectId, ref: 'Post' },
